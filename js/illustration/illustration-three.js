dea4745d90f4efff45bfa66aa975eee80425ee17
rezalmp7 <rezalwahyu10@gmail.com>
2024-12-20 10:08:38 +0700

    3D Test First Commit

---
+++ js/illustration/illustration-three.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+function animate() {
+    requestAnimationFrame(animate);
+	renderer.render( scene, camera );
+}
